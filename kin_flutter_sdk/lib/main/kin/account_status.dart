06febdd5d832e8500a45529a8825099b2d4a5d5b
Andrew <andrewmocol@hotmail.com>
2020-06-16 22:04:20 -0500

    adding some stuff

--- kin_flutter_sdk/lib/main/kin/account_status.dart
+++ kin_flutter_sdk/lib/main/kin/account_status.dart
@@ -1,1 +1,8 @@
-//Placeholder
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+ 
+class _MyAccountStatus extends State<KinAccount> {
+  static const platform = const MethodChannel('com.kinecosystem.kin-sdk/AccountStatus');
+}
