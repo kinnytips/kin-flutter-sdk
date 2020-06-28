3ef5c9fdfbcb0a49ff6a7e87381470c637d8e4e3
Andrew <andrewmocol@hotmail.com>
2020-06-27 20:18:40 -0500

    added back old files

---
+++ kin_flutter_sdk/lib/main/kin/backup_restore.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class _BackupRestoreState extends State<BackupRestore>  {
+  static const platform = const MethodChannel('com.kinecosystem.kin-sdk/BackupRestore');
+  // Backup and Restore
+  String _exportWalletResult = 'awaiting wallet to export.';
+  Future<void> _exportWallet() async {
+    String exportWalletResult;
+    try {
+      exportWalletResult = await platform.invokeMethod('exportWallet');
+    } on PlatformException catch (e) {
+      exportWalletResult = "Failed to export wallet: '${e.message}'.";
+    }
+
+   setState(() {
+      _exportWalletResult = exportWalletResult;
+    });
+  }
+
+  String _importWalletResult;
+  Future<void> _importWallet() async {
+    String importWalletResult;
+    try {
+      importWalletResult = await platform.invokeMethod('importWallet');
+    } on PlatformException catch (e) {
+      importWalletResult = "Failed to import wallet: '${e.message}'.";
+    }
+
+   setState(() {
+      _importWalletResult = importWalletResult;
+    });
+  }
+
+}
