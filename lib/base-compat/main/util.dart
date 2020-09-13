0b9a9d3a891c9af843b280a1ef09767510762803
Andrew <andrewmocol@hotmail.com>
2020-09-12 21:43:43 -0500

    more changes

--- lib/base-compat/main/util.dart
+++ lib/base-compat/main/util.dart
@@ -23,7 +23,7 @@ class FriendBot {
 
   /// Ask the friendly bot to fund your testnet account given by [accountId].
   static Future<bool> fundTestAccount(String accountId) async {
-    var url = "horizon$accountId";
+    var url = "{https://friendbot.stellar.org/?addr=}$accountId";
     return await http
         .get(url, headers: RequestBuilder.headers)
         .then((response) {
