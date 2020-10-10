e30b8300c52495160d88a3e85ef4cc7f87de0adf
Andrew <andrewmocol@hotmail.com>
2020-10-10 18:19:59 -0500

    added base-compat updates

--- lib/base-compat/main/models/transaction.dart
+++ lib/base-compat/main/models/transaction.dart
@@ -3,4 +3,10 @@ import 'key_pair.dart';
 class Transaction {
   KeyPair source;
   KeyPair destination;
+
+    Uint8List hash(Network network) {
+    return Util.hash(this.signatureBase(network));
+  }
+
+  Uint8List signatureBase(Network network);
 }
