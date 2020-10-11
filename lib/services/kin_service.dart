c6585beaeaae39afbc11eef4d681bf1609e674d8
Devayan Sarkar <devayansarkar@gmail.com>
2020-10-11 15:18:30 +0200

    using ago account api

---
+++ lib/services/kin_service.dart
@@ -0,0 +1,18 @@
+import 'package:kin_sdk/models/application/account_creation_response.dart';
+import 'package:kin_sdk/services/agora/account_service.dart';
+
+class KinService {
+  AccountService accountService;
+
+  KinService() {
+    this.accountService = AccountService();
+  }
+
+  Future<AccountCreationResponse> createAccount(String accountId) async {
+    return this.accountService.initiateAccount(accountId);
+  }
+
+  Future<AccountCreationResponse> addAccount(String accountId) async {
+    return this.accountService.initiateAccount(accountId);
+  }
+}
