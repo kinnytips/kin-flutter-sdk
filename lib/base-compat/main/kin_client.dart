462d0d690028a80afaef7c9f124a956dca193ddd
Andrew <andrewmocol@hotmail.com>
2020-10-17 20:17:58 -0500

    WIP building classes

--- lib/base-compat/main/kin_client.dart
+++ lib/base-compat/main/kin_client.dart
@@ -1,5 +1,9 @@
 //Placeholder
 
-class _KinClient extends State<_KinClientState>  {
-  static const platform = const MethodChannel('com.kin.kin_flutter_sdk/kinClient');
+class KinClient {
+  KinClientInternal _delegate;
+
+  KinClient(Context context, Environment environment, String appId, String storeKey, BackupRestore backupRestore, KeyStore keyStore, Storage storage, KinEnvironment kinEnvironment) {
+    _delegate = new KinClientInternal(context, environment, appId, storeKey, backupRestore, keyStore, storage, kinEnvironment);
+  }
 }
