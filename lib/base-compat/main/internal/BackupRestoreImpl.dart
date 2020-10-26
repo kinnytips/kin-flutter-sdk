b4be4670274bd77368ef00131f3d57bca79b57b1
Andrew <andrewmocol@hotmail.com>
2020-10-25 19:51:02 -0500

    add pointycastle

--- lib/base-compat/main/internal/BackupRestoreImpl.dart
+++ lib/base-compat/main/internal/BackupRestoreImpl.dart
@@ -1,9 +1,21 @@
 import 'dart:convert';
+import 'dart:typed_data';
 
+import 'package:flutter_sodium/flutter_sodium.dart';
 import 'package:kin_sdk/base-compat/main/backup_restore.dart';
 import 'package:kin_sdk/base-compat/main/models/key_pair.dart';
 
 class BackupRestoreImpl extends BackupRestore {
+  // ignore: non_constant_identifier_names
+  int SALT_LENGTH_BYTES = 16;
+  // ignore: non_constant_identifier_names
+  int HASH_LENGTH_BYTES = 32;
+
+  ByteData generateRandomBytes(int len) {
+    ByteData byte = new ByteData(len);
+    Sodium.randombytesBuf(len);
+  }
+  
   String exportWallet (KeyPair keyPair, String passphrase) {
     return _exportAccountBackup(keyPair, passphrase).toString();
   }
