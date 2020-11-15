17f8cbc110e07ffb4967ce3d236f68f0cfbd6013
rajesh <rajesh.k.khadka@gmail.com>
2020-11-15 22:57:23 +0545

    add sample data for encoding

--- lib/models/application/kin_helper.dart
+++ lib/models/application/kin_helper.dart
@@ -2,6 +2,7 @@
 //
 import 'dart:convert';
 import 'dart:typed_data';
+import 'dart:convert' show utf8;
 
 const MASK_MAGIC_BYTE_INDICATOR = 3;
 const BIT_OFFSET_MAGIC_BYTE_INDICATOR = 0;
@@ -31,22 +32,33 @@ Uint8List int32bytes(int value) =>
 
 abstract class KinHelper {
   static Uint8List getForeignKeyBytes(String foreignKey) {
-    Uint8List result = Base64Codec().decode(foreignKey).sublist(0, 29);
-    result[28] = result[28] & int32bytes(int.parse("0x3F"))[0];
-    return result;
+    var bytes = base64Decode(foreignKey);
+    List result = bytes.sublist(0, 29);
+    result[28] = result[28] & int.parse("0x3F");
+    return Uint8List(0);
   }
 
+  //
+
   static Uint8List encode() {
-    // const lowerBytes =;
     const version = 0;
     const appIdx = 1;
     const magicByteIndicator = 1;
     const foreignKey = "LlGNZRHhFnMcDk3/oZ62OsPrRXjv0M2sk9YTIAA=";
-    var typeArray = Uint8List(32);
+
+    var byteCountTotal = ((BIT_LENGTH_MAGIC_BYTE_INDICATOR +
+                BIT_LENGTH_VERSION +
+                BIT_LENGTH_TYPE_ID +
+                BIT_LENGTH_APP_IDX +
+                BIT_LENGTH_FOREIGN_KEY) /
+            8.0)
+        .ceil()
+        .toInt();
+    var typeArray = List.filled(byteCountTotal, 0);
 
     // result = {65, 4, 0, 0}
 
-    var result = int32bytes((magicByteIndicator <<
+    var lowerBytes = int32bytes((magicByteIndicator <<
             (BIT_OFFSET_MAGIC_BYTE_INDICATOR & MASK_MAGIC_BYTE_INDICATOR)) |
         (version << BIT_OFFSET_VERSION & MASK_VERSION) |
         (spend << BIT_OFFSET_TYPE_ID & MASK_TYPE_ID) |
@@ -60,14 +72,34 @@ abstract class KinHelper {
             .ceil())
         .toInt();
 
-    List.copyRange(result, 0, typeArray, 0, byteCountLowerBytes);
+    List.copyRange(typeArray, 0, lowerBytes, 0, byteCountLowerBytes);
 
     int byteOfFkStart = (BIT_OFFSET_FOREIGN_KEY / 8.0).floor().toInt();
 
-    typeArray[byteOfFkStart] = typeArray[byteOfFkStart] |
-        (((getForeignKeyBytes(foreignKey)[0] & int.parse("0x3F"))) << 2);
+    Uint8List foreignKeyBytes = getForeignKeyBytes(foreignKey);
 
-    print("lowerBytes: $result");
+    typeArray[byteOfFkStart] = typeArray[byteOfFkStart] |
+        ((foreignKeyBytes[0] & int.parse("0x3F")) << 2).toSigned(8);
+
+    // kotlin expected [46, 81, -115, 101, 17, -31, 22, 115, 28, 14, 77, -1, -95, -98, -74, 58, -61, -21, 69, 120, -17, -48, -51, -84, -109, -42, 19, 32, 0]
+    // dart actual     [46, 81, 141, 101, 17, 225, 22, 115, 28, 14, 77, 255, 161, 158, 182, 58, 195, 235, 69, 120, 239, 208, 205, 172, 147, 214, 19, 32, 0]
+
+    print("foreignKeyBytes $foreignKeyBytes");
+
+    for (var i = byteOfFkStart + 1; i < 2 + foreignKeyBytes.length; i++) {
+      typeArray[i] =
+          typeArray[i] | ((foreignKeyBytes[i - 4] >> 6) & int.parse("0x3"));
+      typeArray[i] = typeArray[i] |
+          ((foreignKeyBytes[i - 3] & int.parse("0x3F") << 2).toSigned(8));
+    }
+    if (foreignKeyBytes.length < 29) {
+      typeArray[byteOfFkStart + foreignKeyBytes.length] =
+          ((foreignKeyBytes[foreignKeyBytes.length - 1]) >> 6) &
+              int.parse("0x3");
+    }
+    //[65, 4, 0, -72, 68, 53, -106, 69, -124, 91, -52, 113, 56, 52, -3, -121, 122, -38, -22, 12, -81, 23, -31, -67, 67, 55, -77, 78, 90, 79, -128, 0]
+    //[65, 4, 0, -72, 80, -115, 102, 17, -32, 23, 112, 29, 12, 76, -3, -93, -98, -74, 58, -64, -21, 71, 121, -19, -45, -49, -81, -110, -42, 19, 32, 0]
+    print("result $typeArray");
 
     return Uint8List(20);
   }
