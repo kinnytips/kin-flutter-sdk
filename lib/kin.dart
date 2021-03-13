9b69a87e5655f92a5d0a4671da6a9a94a60e900f
Devayan Sarkar <devayansarkar@gmail.com>
2021-03-13 16:31:30 +0100

    using new name

--- lib/kin.dart
+++ lib/kin.dart
@@ -1,11 +1,9 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 
-class KinSdk {
-  static const MethodChannel _channel =
-      const MethodChannel('kin_sdk');
+class Kin {
+  static const MethodChannel _channel = const MethodChannel('kin_sdk');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
