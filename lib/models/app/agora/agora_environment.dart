80d48aa257177b7cccc76f1aede2c429eb3e22c6
Devayan Sarkar <devayansarkar@gmail.com>
2021-03-13 18:07:41 +0100

    to toggle between environments

---
+++ lib/models/app/agora/agora_environment.dart
@@ -0,0 +1,7 @@
+class AgoraEnvironment {
+  static String _testEndpoint = "api.agorainfra.dev";
+  static String _productionEndpoint = "api.agorainfra.net";
+
+  static String getEndpoint(bool isProduction) =>
+      isProduction ? _productionEndpoint : _testEndpoint;
+}
