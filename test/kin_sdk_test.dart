4fc54f5d2b9f5e05f8ebdc45d84038c7ffb2ebaa
Devayan Sarkar <devayansarkar@gmail.com>
2021-03-13 18:11:24 +0100

    using new package

--- test/kin_sdk_test.dart
+++ test/kin_sdk_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:kin_sdk/kin_sdk.dart';
+import 'package:kinny/kin.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('kin_sdk');
@@ -18,6 +18,6 @@ void main() {
   });
 
   test('getPlatformVersion', () async {
-    expect(await KinSdk.platformVersion, '42');
+    expect(await Kin.platformVersion, '42');
   });
 }
