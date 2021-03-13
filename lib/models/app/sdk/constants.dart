4cc5fae9e04012ed18181c556f6c285b07d5994a
Devayan Sarkar <devayansarkar@gmail.com>
2021-03-13 18:09:14 +0100

    sdk constants

---
+++ lib/models/app/sdk/constants.dart
@@ -0,0 +1,8 @@
+import 'package:kinny/models/app/interfaces/meta.dart';
+
+class Constants {
+  static String sdkVersion = "0.0.1";
+  static String agoraApiVersion = "v4";
+
+  static get meta => MetaInformation(sdkVersion, agoraApiVersion);
+}
