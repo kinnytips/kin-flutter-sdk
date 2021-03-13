677757e9b57231c6af0bc145c060466f68c04c4e
Devayan Sarkar <devayansarkar@gmail.com>
2021-03-13 18:38:00 +0100

    boiler plate to test out sdk

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,31 +14,20 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
+  String _accountId = 'Unknown';
+  Kin kin = Kin(isProduction: false);
   @override
   void initState() {
     super.initState();
-    initPlatformState();
+    createAccountOnBlocChain();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await Kin.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
+  Future<void> createAccountOnBlocChain() async {
+    await kin.createAccount();
+    String accountId = kin.accountId;
     if (!mounted) return;
-
     setState(() {
-      _platformVersion = platformVersion;
+      _accountId = accountId;
     });
   }
 
@@ -47,10 +36,10 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Example app to test Kin SDK'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: Text('Running on: $_accountId\n'),
         ),
       ),
     );
