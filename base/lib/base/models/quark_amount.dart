1002b4e9454b14d8733e1181dd30b5f0355114d0
Burhan <burhanhaq3@gmail.com>
2021-04-06 11:05:23 -0700

    Added models/quark_amount

--- base/lib/base/models/quark_amount.dart
+++ base/lib/base/models/quark_amount.dart
@@ -1,1 +1,17 @@
-//Placeholder
+import 'package:decimal/decimal.dart';
+
+class QuarkAmount {
+  final int value;
+
+  QuarkAmount(this.value);
+}
+
+const QUARK_CONVERSION_RATE = 100000;
+
+extension KinToQuark on KinAmount {
+  QuarkAmount toQuarks() => QuarkAmount((value * (Decimal.fromInt(QUARK_CONVERSION_RATE))).toInt());
+}
+
+extension QuarkToKin on QuarkAmount {
+  KinAmount toKin() => KinAmount(Decimal.fromInt(value) / Decimal.fromInt(QUARK_CONVERSION_RATE));
+}
