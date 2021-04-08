6142f556c7c38998a868300fa5d10f1b6b8e15eb
gmpassos <gracilianomp@gmail.com>
2021-04-08 19:09:24 -0300

    - Implement `kin_account.dart`: `KinAccount`, `KinAccountId`, `KinAccountStatus`. - Added `Key.asPublicKey`.

--- base/lib/base/models/kin_account.dart
+++ base/lib/base/models/kin_account.dart
@@ -1,1 +1,69 @@
-//Placeholder
+import 'dart:typed_data';
+
+import 'package:kinny/base/tools/base58.dart';
+import 'package:kinny/stellarfork/key_pair.dart';
+
+import 'key.dart';
+import 'kin_balance.dart';
+
+class KinAccountId {
+  final Uint8List value;
+
+  KinAccountId(this.value);
+
+  KinAccountId.fromIdString(String accountId)
+      : this(KeyPair.fromAccountId(accountId).publicKey);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is KinAccountId && runtimeType == other.runtimeType && value == other.value;
+
+  @override
+  int get hashCode => value.hashCode;
+
+  String toString() {
+    return "Id(value=${stellarBase32Encode()}, b58=${base58Encode()})";
+  }
+
+  String stellarBase32Encode() => toKeyPair().accountId;
+
+  String base58Encode() => Base58().encode(value);
+
+  KeyPair toKeyPair() => KeyPair.fromPublicKey(value);
+}
+
+class KinAccountStatus {
+  final int value;
+
+  KinAccountStatus._(this.value);
+}
+
+class KinAccountStatusUnregistered extends KinAccountStatus {
+  static final KinAccountStatusUnregistered instance = KinAccountStatusUnregistered._();
+
+  KinAccountStatusUnregistered._() : super._(0);
+
+  factory KinAccountStatusUnregistered() => instance;
+}
+
+class KinAccountStatusRegistered extends KinAccountStatus {
+  int sequence;
+
+  KinAccountStatusRegistered(this.sequence) : super._(1);
+}
+
+class KinAccount {
+  final Key key;
+  final KinAccountId id;
+  final List<PublicKey> tokenAccounts;
+  final KinBalance balance;
+  final KinAccountStatus status;
+
+  KinAccount(this.key,
+      {KinAccountId id, List<PublicKey> tokenAccounts, KinBalance balance, KinAccountStatus status})
+      : id = id ?? KinAccountId(key.asPublicKey().value),
+        tokenAccounts = tokenAccounts ?? <PublicKey>[],
+        balance = balance ?? KinBalance(),
+        status = status ?? KinAccountStatusUnregistered();
+}
