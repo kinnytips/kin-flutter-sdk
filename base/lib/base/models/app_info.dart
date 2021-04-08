8715e7882c6ce4a3d0dc5756baf243dd785fca54
Burhan <burhanhaq3@gmail.com>
2021-04-08 14:53:37 -0700

    Added app_info.dart

--- base/lib/base/models/app_info.dart
+++ base/lib/base/models/app_info.dart
@@ -1,1 +1,22 @@
-//Placeholder
+import 'package:kinny/base/models/appidx.dart';
+import 'package:kinny/base/network/models/kin_account.dart';
+
+///
+/// @param appIndex - An assigned integer for this application. Make sure you are using the correct one for your app!
+/// @param kinAccountId - This [KinAccount.Id] is for the account you wish to collect Kin from by default.
+/// @property appName - The name of your App that can be used to display to a user in certain contexts.
+/// @property appIconResourceId - The ResourceId of your app's icon.
+///
+class AppInfo {
+  final AppIdx appIndex;
+  final KinAccountId kinAccountId;
+  final String appName;
+  final int appIconResourceId;
+
+  AppInfo(
+    this.appIndex,
+    this.kinAccountId,
+    this.appName,
+    this.appIconResourceId,
+  );
+}
