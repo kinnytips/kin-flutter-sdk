b8333491bb2dabb660e261146e24d3ce2ddcb0d6
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-17 20:15:11 +0200

    Response model for retreiving account response

--- base/lib/models/app/account/retrieve_kin_account_response.dart
+++ base/lib/models/app/account/retrieve_kin_account_response.dart
@@ -3,10 +3,10 @@ import 'package:kinny/models/app/account/account_response.dart';
 import 'package:kinny/models/app/interfaces/meta.dart';
 import 'package:kinny/models/app/interfaces/status.dart';
 
-class GetKinAccountResponse extends AccountResponse {
+class RetrieveKinAccountResponse extends AccountResponse {
   final GetAccountInfoResponse_Result result;
 
-  GetKinAccountResponse(AccountInfo accountInfo, this.result,
+  RetrieveKinAccountResponse(AccountInfo accountInfo, this.result,
       Status operationStatus, MetaInformation meta)
       : super(accountInfo, operationStatus, meta);
 }
