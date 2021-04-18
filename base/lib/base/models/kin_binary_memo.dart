8dd49ef6e0fbe5fa36710403bee149a1719b1b8e
gmpassos <gracilianomp@gmail.com>
2021-04-18 15:06:43 -0300

    Finish `kin_binary_memo.dart` and fix `kin_payment_item.dart`.

--- base/lib/base/models/kin_binary_memo.dart
+++ base/lib/base/models/kin_binary_memo.dart
@@ -1,145 +1,145 @@
 import 'dart:convert';
 import 'dart:math';
-import 'dart:scalarlist';
-import 'package:flutter/services.dart';
+import 'dart:typed_data';
+
+import 'package:kin_base/base/tools/extensions.dart';
+
+import 'kin_memo.dart';
 
 class KinBinaryMemo {
-  KinBinaryMemo(int magicByteIndicator, int version, TransferType typeId, int appIdx, String foreignKey) {
 
-  }
-  // ignore: non_constant_identifier_names
-  int MASK_MAGIC_BYTE_INDICATOR = 0x3;
-  // ignore: non_constant_identifier_names
-  int MASK_VERSION = 0x1C;
-  // ignore: non_constant_identifier_names
-  int MASK_TYPE_ID = 0x3E0;
-  // ignore: non_constant_identifier_names
-  int MASK_APP_IDX = 0x3FFFC00;
+  final int magicByteIndicator ;
+  final int  version;
+  final TransferType typeId ;
+  final int  appIdx;
+  final String foreignKey;
+
+  KinBinaryMemo(this.magicByteIndicator, this.version, this.typeId, this.appIdx, this.foreignKey);
 
   // ignore: non_constant_identifier_names
-  int BIT_LENGTH_MAGIC_BYTE_INDICATOR = 2;
+  static const int MASK_MAGIC_BYTE_INDICATOR = 0x3;
   // ignore: non_constant_identifier_names
-  int BIT_LENGTH_VERSION = 3;
+  static const int MASK_VERSION = 0x1C;
   // ignore: non_constant_identifier_names
-  int BIT_LENGTH_TYPE_ID = 5;
+  static const int MASK_TYPE_ID = 0x3E0;
   // ignore: non_constant_identifier_names
-  int BIT_LENGTH_APP_IDX = 16;
-  // ignore: non_constant_identifier_names
-  int BIT_LENGTH_FOREIGN_KEY = 230;
+  static const int MASK_APP_IDX = 0x3FFFC00;
 
   // ignore: non_constant_identifier_names
-  int BIT_OFFSET_MAGIC_BYTE_INDICATOR = 0;
+  static const int BIT_LENGTH_MAGIC_BYTE_INDICATOR = 2;
   // ignore: non_constant_identifier_names
-  int BIT_OFFSET_VERSION() {
-    return BIT_LENGTH_MAGIC_BYTE_INDICATOR;
-  }
+  static const int BIT_LENGTH_VERSION = 3;
   // ignore: non_constant_identifier_names
-  int BIT_OFFSET_TYPE_ID() {
-    return (BIT_OFFSET_VERSION.call() + BIT_LENGTH_VERSION);
-  } 
+  static const int BIT_LENGTH_TYPE_ID = 5;
   // ignore: non_constant_identifier_names
-  int BIT_OFFSET_APP_IDX() {
-    return (BIT_OFFSET_TYPE_ID.call() + BIT_LENGTH_TYPE_ID);
-  }
+  static const int BIT_LENGTH_APP_IDX = 16;
   // ignore: non_constant_identifier_names
-  int BIT_OFFSET_FOREIGN_KEY()
-  {
-    return (BIT_OFFSET_APP_IDX.call() + BIT_LENGTH_APP_IDX);
-  } 
+  static const int BIT_LENGTH_FOREIGN_KEY = 230;
 
   // ignore: non_constant_identifier_names
-  int BYTE_COUNT_LOWER_BYTES() {
-    return ((BIT_LENGTH_MAGIC_BYTE_INDICATOR + BIT_LENGTH_VERSION + BIT_LENGTH_TYPE_ID + BIT_LENGTH_APP_IDX) / 8).ceil().toInt();
-  }
+  static const int BIT_OFFSET_MAGIC_BYTE_INDICATOR = 0;
   // ignore: non_constant_identifier_names
-  int BYTE_COUNT_FOREIGN_KEY() {
-    return (BIT_LENGTH_FOREIGN_KEY / 8).ceil().toInt();
-  }
+  static const int BIT_OFFSET_VERSION = BIT_LENGTH_MAGIC_BYTE_INDICATOR;
+
   // ignore: non_constant_identifier_names
-  int BYTE_COUNT_TOTAL() {
-    return ((BIT_LENGTH_MAGIC_BYTE_INDICATOR + BIT_LENGTH_VERSION + BIT_LENGTH_TYPE_ID + BIT_LENGTH_APP_IDX + BIT_LENGTH_FOREIGN_KEY) / 8).ceil().toInt();
-  }
-  
+  static const int BIT_OFFSET_TYPE_ID = BIT_OFFSET_VERSION + BIT_LENGTH_VERSION ;
+
   // ignore: non_constant_identifier_names
-  int BYTE_OF_FK_START()
-  {
-    return (BIT_OFFSET_FOREIGN_KEY.call() / 8).floor().toInt();
-  } 
+  static const int BIT_OFFSET_APP_IDX = BIT_OFFSET_TYPE_ID + BIT_LENGTH_TYPE_ID;
 
-  //TODO: Finish converting this
-  // KinBinaryMemo decode(ByteData bytes)) = with(bytes.subByteArray(0, 4).byteArrayToInt()) {
-  //     val magicByteIndicator =
-  //         this and MASK_MAGIC_BYTE_INDICATOR ushr BIT_OFFSET_MAGIC_BYTE_INDICATOR
-  //     val version = this and MASK_VERSION ushr BIT_OFFSET_VERSION
-  //     val typeId = TransferType.fromValue(this and MASK_TYPE_ID ushr BIT_OFFSET_TYPE_ID)
-  //     val appIdx = this and MASK_APP_IDX ushr BIT_OFFSET_APP_IDX
+  // ignore: non_constant_identifier_names
+  static const int BIT_OFFSET_FOREIGN_KEY = BIT_OFFSET_APP_IDX + BIT_LENGTH_APP_IDX ;
 
-  //     val foreignKey = ByteArray(29)
-  //     for (i in 0..27) {
-  //         foreignKey[i] = foreignKey[i] or (bytes[i + 3] ushr 2) and 0x3F
-  //         foreignKey[i] = foreignKey[i] or ((bytes[i + 4] and 0x3) shl 6)
-  //     }
-  //     foreignKey[28] = foreignKey[28] or ((bytes[31] ushr 2) and 0x3F)
+  // ignore: non_constant_identifier_names
+  static final int BYTE_COUNT_LOWER_BYTES = ((BIT_LENGTH_MAGIC_BYTE_INDICATOR + BIT_LENGTH_VERSION + BIT_LENGTH_TYPE_ID + BIT_LENGTH_APP_IDX) / 8).ceil() ;
 
-  //     KinBinaryMemo(
-  //         magicByteIndicator,
-  //         version,
-  //         typeId,
-  //         appIdx,
-  //         Base64().encodeAsString(foreignKey)
-  //     )
-  //   }
+  // ignore: non_constant_identifier_names
+  static final int BYTE_COUNT_FOREIGN_KEY = (BIT_LENGTH_FOREIGN_KEY / 8).ceil();
 
-    ByteData foreignKeyBytes() {
-        base64.decode(foreignKey)
-        
-            subByteArray(0, 29)
-            .apply { set(28, get(28) and 0x3F.toByte()) }
+  // ignore: non_constant_identifier_names
+  static final int BYTE_COUNT_TOTAL = ((BIT_LENGTH_MAGIC_BYTE_INDICATOR + BIT_LENGTH_VERSION + BIT_LENGTH_TYPE_ID + BIT_LENGTH_APP_IDX + BIT_LENGTH_FOREIGN_KEY) / 8).ceil() ;
+
+  // ignore: non_constant_identifier_names
+  static final int BYTE_OF_FK_START = (BIT_OFFSET_FOREIGN_KEY / 8).floor() ;
+
+  KinBinaryMemo decode(Uint8List bytes) {
+    var n = ByteData.view(bytes.buffer).getInt32(0) ;
+
+    var magicByteIndicator = n & ( (MASK_MAGIC_BYTE_INDICATOR & 0xFFFFFFFF) >> BIT_OFFSET_MAGIC_BYTE_INDICATOR) ;
+    var version = n & ( (MASK_VERSION & 0xFFFFFFFF) >> BIT_OFFSET_VERSION);
+
+    var typeId = TransferTypeUtil.getType( n & ((MASK_TYPE_ID & 0xFFFFFFFF) >> BIT_OFFSET_TYPE_ID) );
+
+    var appIdx = n & ((MASK_APP_IDX & 0xFFFFFFFF) >> BIT_OFFSET_APP_IDX) ;
+
+    var foreignKey = Uint8List(29);
+
+    for (var i = 0; i <= 27; ++i) {
+      foreignKey[i] = foreignKey[i] | ( (bytes[i + 3] & 0xFFFFFFFF) >> 2) & 0x3F ;
+      foreignKey[i] = foreignKey[i] | ( ( (bytes[i + 4] & 0xFFFFFFFF) >> 0x3) << 6);
     }
 
-  //   /**
-  //    * Fields below are packed from LSB to MSB order:
-  //    *
-  //    * magicByteIndicator    2 bits   | < 4
-  //    * version               3 bits   | < 8
-  //    * typeId                5 bits   | < 32
-  //    * appIdx                16 bits  | < 65,536
-  //    * foreignKey            230 bits | Often a SHA-224 of an [InvoiceList] but could be anything
-  //    */
-  //   fun encode(): ByteArray = ByteArray(BYTE_COUNT_TOTAL).apply {
-  //       val lowerBytes =
-  //           ((magicByteIndicator shl BIT_OFFSET_MAGIC_BYTE_INDICATOR and MASK_MAGIC_BYTE_INDICATOR)
-  //                   or (version shl BIT_OFFSET_VERSION and MASK_VERSION)
-  //                   or (typeId.value shl BIT_OFFSET_TYPE_ID and MASK_TYPE_ID)
-  //                   or (appIdx shl BIT_OFFSET_APP_IDX and MASK_APP_IDX))
-  //               .intToByteArray()
-  //       System.arraycopy(lowerBytes, 0, this, 0, BYTE_COUNT_LOWER_BYTES)
+    foreignKey[28] = foreignKey[28] | ( ( (bytes[31] & 0xFFFFFFFF) >> 2) & 0x3F);
 
-  //       this[BYTE_OF_FK_START] = this[BYTE_OF_FK_START] or ((foreignKeyBytes[0] and 0x3F) shl 2)
-  //       // insert the rest of the fk. since each loop references fk[n] and fk[n+1],
-  //       // the upper bound is offset by 3 instead of 4.
-  //       for (i in (BYTE_OF_FK_START + 1)..(2 + foreignKeyBytes.size)) {
-  //           this[i] = this[i] or ((foreignKeyBytes[i - 4] ushr 6) and 0x3)
-  //           this[i] = this[i] or (foreignKeyBytes[i - 3] and 0x3F shl 2)
-  //       }
-  //       // if the foreign key is less than 29 bytes,
-  //       // the last 2 bits of the FK can be included in the memo
-  //       if (foreignKeyBytes.size < 29) {
-  //           this[BYTE_OF_FK_START + foreignKeyBytes.size] =
-  //               ((foreignKeyBytes[foreignKeyBytes.size - 1] ushr 6)) and 0x3
-  //       }
-  //   }
+    return KinBinaryMemo(
+        magicByteIndicator,
+        version,
+        typeId,
+        appIdx,
+        base64.encode(foreignKey)
+    );
+  }
+
+  Uint8List _foreignKeyBytes ;
+
+  Uint8List get foreignKeyBytes {
+    if (_foreignKeyBytes == null) {
+      var bs = base64.decode(foreignKey).sublist(0, 29) ;
+      bs[28] = bs[28] & 0x3F ;
+      _foreignKeyBytes = bs ;
+    }
+    return _foreignKeyBytes ;
+  }
+
+  Uint8List encode() {
+    var bs = Uint8List(BYTE_COUNT_TOTAL) ;
+
+    var lowerBytes =
+    (
+        (magicByteIndicator << BIT_OFFSET_MAGIC_BYTE_INDICATOR & MASK_MAGIC_BYTE_INDICATOR)
+        | (version << BIT_OFFSET_VERSION & MASK_VERSION)
+        | (typeId.getValue() << BIT_OFFSET_TYPE_ID & MASK_TYPE_ID)
+        | (appIdx << BIT_OFFSET_APP_IDX & MASK_APP_IDX)
+    ).toInt32Uint8List() ;
+
+    bs.setRange(0, BYTE_COUNT_LOWER_BYTES, lowerBytes);
+
+    bs[BYTE_OF_FK_START] = bs[BYTE_OF_FK_START] | ((foreignKeyBytes[0] & 0x3F) << 2);
+
+    // insert the rest of the fk. since each loop references fk[n] and fk[n+1],
+    // the upper bound is offset by 3 instead of 4.
+    for (var i = BYTE_OF_FK_START + 1; i <= 2 + foreignKeyBytes.length; ++i) {
+      bs[i] = bs[i] | (( (foreignKeyBytes[i - 4] & 0xFFFFFFFF) >> 6) & 0x3);
+      bs[i] = bs[i] | (foreignKeyBytes[i - 3] & 0x3F << 2);
+    }
+
+    if (foreignKeyBytes.length < 29) {
+      bs[BYTE_OF_FK_START + foreignKeyBytes.length] = (( (foreignKeyBytes[foreignKeyBytes.length - 1] & 0xFFFFFFFF) >> 6)) & 0x3;
+    }
+
+    return bs ;
+  }
 
   toKinMemo(){
     return KinMemo(encode());
-  } 
+  }
 
   String toString() {
       return "AgoraMemo(magicByteIndicator=$magicByteIndicator, " +
               "version=$version, " +
-              "typeId=${typeId.value}, " +
+              "typeId=${typeId.getValue()}, " +
               "appIdx=$appIdx, " +
-              "foreignKey='$foreignKey')"
+              "foreignKey='$foreignKey')";
   }
 }
 
@@ -158,37 +158,37 @@ extension TransferTypeUtil on TransferType {
       case 1: return TransferType.earn;
       case 2: return TransferType.spend;
       case 3: return TransferType.p2p;
+      default: throw StateError("Invalid value: $value");
     }
   }
 
-    static int getValue(TransferType type) {
-    switch(type) {
+  int getValue() {
+    switch(this) {
       case TransferType.unknown: return -1;
       case TransferType.none: return 0;
       case TransferType.earn: return 1;
       case TransferType.spend: return 2;
       case TransferType.p2p: return 3;
+      default: return -1;
     }
   }
 }
 
-class Builder {
+class KinBinaryMemoBuilder {
+  int appIdx;
   int magicByteIndicator;
   int version;
-  int appIdx;
 
   TransferType _typeId;
-  ByteData _foreignKeyBytes;
+  Uint8List _foreignKeyBytes;
 
+  KinBinaryMemoBuilder(this.appIdx, [this.magicByteIndicator = 1, this.version = 0]) ;
 
-  Builder(this.appIdx, [this.magicByteIndicator = 1, this.version = 0]) { 
-  }
-
-  setTransferType(TransferType typeId) {
+  void setTransferType(TransferType typeId) {
     this._typeId = typeId;
   }
 
-  setForeginKey(ByteData foreignKeyBytes) {
+  void setForeignKey(Uint8List foreignKeyBytes) {
     this._foreignKeyBytes = foreignKeyBytes;
   }
 
@@ -198,14 +198,15 @@ class Builder {
     }
 
     if (_foreignKeyBytes == null) {
-      _foreignKeyBytes = ByteData(0);
+      _foreignKeyBytes = Uint8List(0);
     }
 
     int _typeIdMaxSize = pow(2, 5).toInt();
-    if(TransferTypeUtil.getValue(_typeId) < 0 || TransferTypeUtil.getValue(_typeId) >= _typeIdMaxSize) {
-      int typeValue = TransferTypeUtil.getValue(_typeId);
+    var typeIdValue = _typeId.getValue();
+
+    if(typeIdValue < 0 || typeIdValue >= _typeIdMaxSize) {
       throw KinBinaryMemoFormatException(
-        "typeId of $typeValue invalid! must be " +
+        "typeId of $typeIdValue invalid! must be " +
         "larger than zero and less than $_typeIdMaxSize");
     }
 
@@ -233,24 +234,18 @@ class Builder {
       );
     }
 
-    ByteData foreignKeyBytesPadded = ByteData(BYTE_COUNT_FOREIGN_KEY);
+    var foreignKeyBytesPadded = Uint8List(KinBinaryMemo.BYTE_COUNT_FOREIGN_KEY);
 
-    //TODO: IDK HOW TO DO THIS
-      // Pad with zeros or truncate foreignKeyBytes
-      // ByteData foreignKeyBytesPadded = ByteData(BYTE_COUNT_FOREIGN_KEY)
-      // System.arraycopy(
-      //     foreignKeyBytes!!, 0, foreignKeyBytesPadded, 0,
-      //     min(foreignKeyBytesPadded.size, foreignKeyBytes!!.size)
-      // )
-      // trim last two bits, they don't fit
-      // foreignKeyBytesPadded. = foreignKeyBytesPadded[28] + 0x3F.toByte()
+    foreignKeyBytesPadded.setRange(0, min(foreignKeyBytesPadded.length, _foreignKeyBytes.length), _foreignKeyBytes);
+
+    foreignKeyBytesPadded[28] = foreignKeyBytesPadded[28] & 0x3F ;
 
     return KinBinaryMemo(
         magicByteIndicator,
         version,
         _typeId,
         appIdx,
-        base64.encode(foreignKeyBytesPadded.buffer.asUint8List())
+        base64.encode(foreignKeyBytesPadded)
     );
   }
 }
