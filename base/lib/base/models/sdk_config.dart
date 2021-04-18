8d2f9598e8851916726ef7cf8cda155f1d80b757
Burhan <burhanhaq3@gmail.com>
2021-04-17 23:22:58 -0700

    Added incomplete file: sdk_config.dart

--- base/lib/base/models/sdk_config.dart
+++ base/lib/base/models/sdk_config.dart
@@ -1,1 +1,7 @@
-//Placeholder
+class SDKConfig {
+  // todo: set correct values
+  static const platform = 'JVM';
+  static const versionString = '0.4.5';
+  // todo convert below to Dart
+  // val systemUserAgent by lazy { System.getProperty("http.agent") ?: "JVM/unspecified" }
+}
