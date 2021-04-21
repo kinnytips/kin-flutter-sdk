07f0a858a98fc4b0abd9d815ba8089725b45524f
Andrew <andrewmocol@hotmail.com>
2021-04-20 22:16:03 -0500

    starting kinAccountContext

--- base/lib/base/kin_account_context.dart
+++ base/lib/base/kin_account_context.dart
@@ -1,1 +1,31 @@
-//Placeholder
+import 'package:kin_base/base/models/kin_balance.dart';
+import 'package:kin_base/stellarfork/operation.dart';
+import 'package:meta/meta.dart';
+
+import 'models/kin_account.dart';
+
+class ObservationMode {
+  ObservationMode(int value) {}
+
+
+}
+
+class Passive extends ObservationMode {
+    Passive._() : super(0);
+}
+
+class Active extends ObservationMode {
+    Active._() : super(1);
+}
+
+class ActiveNewOnly extends ObservationMode {
+    ActiveNewOnly._() : super(2);
+}
+
+abstract class KinAccountReadOperationsAltIdioms {
+  getAccount(Future<KinAccount> accountCallback, [bool forceUpdate = false]);
+
+  Future<KinBalance> observeBalance(ValueListener<KinBalance> balanceListener, [ObservationMode = Passive]);
+
+  clearStorage(Callback<bool> clearCompleteCallback);
+}
--- base/lib/base/network/api/kin_transaction_api.dart
+++ base/lib/base/network/api/kin_transaction_api.dart
@@ -1,32 +1,3 @@
 class KinTransactionApi {
-  GetTransactionHistoryRequest
 
-  {
-
-  KinAccount.Id accountId,
-      val
-
-  pagingToken
-
-      :
-
-  KinTransaction.PagingToken?
-
-  =
-
-  null
-
-  ,
-
-  val
-
-  order
-
-      :
-
-  Order
-
-  =
-
-  Order.Descending
-}}
+}
