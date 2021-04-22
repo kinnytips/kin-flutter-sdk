25afd0a58a3a9aed68d8878bf8a7c89d56c6abe8
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-22 18:46:05 +0200

    Adding example app to be able to test the app

---
+++ base/example/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:kin_base/kin.dart';
+import 'package:kin_base/models/app/interfaces/status.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _output = 'Unknown';
+  Kin kin = Kin(isProduction: false);
+  @override
+  void initState() {
+    super.initState();
+    createAccountOnBlocChain();
+  }
+
+  Future<void> createAccountOnBlocChain() async {
+    var response = await kin.createAccount();
+    String output;
+    if (response.operationStatus.result == Result.FAIL) {
+      output = response.operationStatus.message;
+    } else {
+      output = kin.accountId;
+    }
+    if (!mounted) return;
+    setState(() {
+      _output = output;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Example app to test Kin SDK'),
+        ),
+        body: Center(
+          child: Text('Result : $_output\n'),
+        ),
+      ),
+    );
+  }
+}
