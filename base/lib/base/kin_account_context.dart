b75e44631f56b5b15ce71e0418b665a66c7e57b1
gmpassos <gracilianomp@gmail.com>
2021-04-24 07:11:05 -0300

    partial implementation of `kin_account_context.dart`: - only interfaces for now.

--- base/lib/base/kin_account_context.dart
+++ base/lib/base/kin_account_context.dart
@@ -1,31 +1,120 @@
 import 'package:kin_base/base/models/kin_balance.dart';
-import 'package:kin_base/stellarfork/operation.dart';
-import 'package:meta/meta.dart';
+import 'package:kin_base/base/tools/observers.dart';
+import 'package:kin_base/stellarfork/xdr/xdr_signing.dart';
 
+import 'models/account_spec.dart';
+import 'models/appidx.dart';
+import 'models/invoices.dart';
 import 'models/kin_account.dart';
+import 'models/kin_amount.dart';
+import 'models/kin_binary_memo.dart';
+import 'models/kin_memo.dart';
+import 'models/kin_payment.dart';
+import 'models/kin_payment_item.dart';
+import 'models/quark_amount.dart';
+import 'models/transaction_hash.dart';
+import 'network/services/app_info_providers.dart';
 
-class ObservationMode {
-  ObservationMode(int value) {}
-
-
+enum ObservationMode {
+  Passive,
+  Active,
+  ActiveNewOnly,
 }
 
-class Passive extends ObservationMode {
-    Passive._() : super(0);
-}
-
-class Active extends ObservationMode {
-    Active._() : super(1);
-}
-
-class ActiveNewOnly extends ObservationMode {
-    ActiveNewOnly._() : super(2);
+extension ObservationModeExtension on ObservationMode {
+  int get value => this.index ;
 }
 
 abstract class KinAccountReadOperationsAltIdioms {
-  getAccount(Future<KinAccount> accountCallback, [bool forceUpdate = false]);
 
-  Future<KinBalance> observeBalance(ValueListener<KinBalance> balanceListener, [ObservationMode = Passive]);
+  void getAccount(Callback<KinAccount> accountCallback, { bool forceUpdate = false }) ;
 
-  clearStorage(Callback<bool> clearCompleteCallback);
+  Observer<KinBalance> observeBalance(ValueListener<KinBalance> balanceListener, { ObservationMode mode = ObservationMode.Passive });
+
+  void clearStorage(Callback<bool> clearCompleteCallback);
+}
+
+abstract class KinAccountReadOperations extends KinAccountReadOperationsAltIdioms {
+
+  Future<KinAccount> getAccountAsFutureMayForceUpdate({ bool forceUpdate = false }) ;
+
+  Future<KinAccount> getAccountAsFuture() ;
+
+  Observer<KinBalance> observeBalanceNoBalanceListener( { ObservationMode mode = ObservationMode.Passive }) ;
+
+  Future<bool> clearStorageNoCallback();
+
+}
+
+abstract class KinPaymentReadOperationsAltIdioms {
+
+  ListObserver<KinPayment> observePayments(ValueListener<List<KinPayment>> paymentsListener, { ObservationMode mode = ObservationMode.Passive });
+
+  void getPaymentsForTransactionHash(TransactionHash transactionHash, Callback<List<KinPayment>> paymentsCallback);
+
+}
+
+
+abstract class  KinPaymentReadOperations extends KinPaymentReadOperationsAltIdioms {
+
+  Future<QuarkAmount> calculateFee(int numberOfOperations);
+
+  ListObserver<KinPayment> observePaymentsNoPaymentsListener({ObservationMode mode = ObservationMode.Passive});
+
+  Future<List<KinPayment>> getPaymentsForTransactionHashNoCallback(TransactionHash transactionHash);
+}
+
+abstract class KinPaymentWriteOperationsAltIdioms {
+  void sendKinPayment(
+      KinAmount amount,
+      KinAccountId destinationAccount,
+      KinMemo memo /* = KinMemo.none */,
+      Invoice invoice,
+      Callback<KinPayment> paymentCallback);
+
+  void sendKinPayments(
+      List<KinPaymentItem> payments,
+      KinMemo memo /* = KinMemo.none */,
+      Callback<List<KinPayment>> paymentsCallback);
+}
+
+
+abstract class KinPaymentWriteOperations extends KinPaymentWriteOperationsAltIdioms {
+
+  AppInfoProvider appInfoProvider ;
+
+  Future<KinPayment> payInvoice(Invoice invoice,
+      KinAccountId destinationAccount,
+      AppIdx processingAppIdx
+      /* = appInfoProvider?.appInfo?.appIndex?: throw RuntimeException("Need to specify an AppIdx") */,
+      TransferType type /* = TransferType.spend */);
+
+  Future<KinPayment> sendKinPaymentToAccount(KinAmount amount,
+      KinAccountId destinationAccount,
+      KinMemo memo /* = KinMemo.NONE */,
+      Invoice invoice);
+
+  Future<List<KinPayment>> sendKinPaymentsByAccountSpec(
+      List<KinPaymentItem>payments,
+      KinMemo memo /*= KinMemo.NONE */,
+      AccountSpec sourceAccountSpec /* = AccountSpec.Preferred*/,
+      AccountSpec destinationAccountSpec /*= AccountSpec.Preferred */,);
+
+  @Deprecated("Don't use this version of sendKinPayments")
+  Future<List<KinPayment>> sendKinPaymentsWithAdditionalSignatures(
+    List<KinPaymentItem> payments,
+    KinMemo memo,
+    AccountSpec sourceAccountSpec,
+    AccountSpec destinationAccountSpec,
+    List<XdrDecoratedSignature> additionalSignatures,
+    QuarkAmount feeOverride,
+  );
+
+  Future<List<KinPayment>> sendKinTransaction(
+      Future<KinTransaction> Function() buildTransaction);
+}
+
+
+abstract class  KinAccountContextReadOnly extends KinAccountReadOperations implements KinPaymentReadOperations {
+  KinAccountId accountId ;
 }
