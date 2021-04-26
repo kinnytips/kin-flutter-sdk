b11793955a7d5f20f2e0766217d58beb9750e99f
Andrew <andrewmocol@hotmail.com>
2021-04-25 21:10:16 -0500

    WIP managedservereventsteam

--- base/lib/base/tools/managed_server_sent_event_stream.dart
+++ base/lib/base/tools/managed_server_sent_event_stream.dart
@@ -1,3 +1,35 @@
-class ManagedServerSentEventStream {
-  
+import 'package:kin_base/stellarfork/requests/request_builder.dart';
+import 'package:sse/server/sse_handler.dart';
+
+class ManagedServerSentEventStream<ResponseType> {
+  ManagedServerSentEventStream(StreamingProtocol<ResponseType> requestBuilder);
+
+  var _lock;
+  List<EventListener<ResponseType>> listeners;
+  SseConnection _connection = null; 
+  ResponseType _lastReceivedResponse = null;
+  var listener = ResponseTypeEventListener();
+
+  addListener(EventListener<ResponseType> listener) {
+    listeners.add(listener);
+    _lastReceivedResponse
+      ..{ listener.onEvent(this) };
+  }
+}
+
+class ResponseTypeEventListener implements EventListener<ResponseType> {
+  onEvent(ResponseType data){
+                // listeners.indices
+                // .asSequence()
+                // .map { listeners[it] }
+                // .forEach { it.onEvent(data) }
+  }
+}
+
+typedef T LetCallback<T>(T value);
+
+T let<T>(T value, LetCallback<T> cb) {
+  if (value != null) {
+    return cb(value);
+  }
 }
