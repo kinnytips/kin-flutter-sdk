31b24e8930cf6fe8e91b24de9739e6ec4ece0b64
Andrew <andrewmocol@hotmail.com>
2021-04-30 01:58:25 -0500

    WIP horizon

--- base/lib/base/network/api/horizon/horizon_kin_api.dart
+++ base/lib/base/network/api/horizon/horizon_kin_api.dart
@@ -1,13 +1,29 @@
+import 'dart:html';
 import 'dart:io';
 
 import 'package:kin_base/base/models/kin_account.dart';
+import 'package:kin_base/base/network/api/horizon/kin_json_api.dart';
 import 'package:kin_base/base/tools/managed_server_sent_event_stream.dart';
+import 'package:kin_base/base/tools/observers.dart';
 import 'package:kin_base/stellarfork/responses/account_response.dart';
+import 'package:kin_base/stellarfork/responses/transaction_response.dart';
 
-class HorizonKinApi {
-  HorizonKinApi(ApiConfig environment, HttpClient httpClient) {}
+class HorizonKinApi implements KinJsonApi {
+  HorizonKinApi(ApiConfig environment, HttpClient httpClient) : super(environment, httpClient);
 
   static const CURSOR_FUTURE_ONLY = "now";
 
   _accountStreams = new Map<KinAccountId, ManagedServerSentEventStream<AccountResponse>>();
+  
+  _transactionStreams = new Map<KinAccountId, ManagedServerSentEventStream<TransactionResponse>>();
+
+  Observer<KinAccount> streamAccount(KinAccountId kinAccountId) {
+    var accountStream = _accountStreams[kinAccountId] ?? ManagedServerSentEventStream<AccountResponse>(server.accounts().forAccount(kinAccountId.toKeyPair()));
+
+    _accountStreams[kinAccountId] = accountStream;
+
+    EventListener<AccountResponse> listener = null;
+
+    return ValueSubject<KinAccount>()
+  }
 }
