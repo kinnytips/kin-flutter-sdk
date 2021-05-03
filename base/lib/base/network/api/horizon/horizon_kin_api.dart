0163252840df939cc6edc99e0cf692f536273363
Andrew <andrewmocol@hotmail.com>
2021-05-02 21:58:01 -0500

    WIP horizon

--- base/lib/base/network/api/horizon/horizon_kin_api.dart
+++ base/lib/base/network/api/horizon/horizon_kin_api.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:kin_base/base/models/kin_account.dart';
 import 'package:kin_base/base/network/api/horizon/kin_json_api.dart';
+import 'package:kin_base/base/network/api/kin_transaction_api.dart';
 import 'package:kin_base/base/tools/managed_server_sent_event_stream.dart';
 import 'package:kin_base/base/tools/observers.dart';
 import 'package:kin_base/stellarfork/responses/account_response.dart';
@@ -26,6 +27,28 @@ class HorizonKinApi implements KinJsonApi {
 
     var subject = ValueSubject<KinAccount>();
 
+    listener = EventListener<AccountResponse>() {
+      onEvent(AccountResponse data) {
+        subject.onNext(data.kinAccount());
+      }
+    };
+
+    _accountStreams.addListener(listener);
+
+    subject.doOnDisposed(() { 
+      _accountStreams.removeListener(listener);
+      _accountStreams[kinAccountId] = null;
+    });
+      
+    return subject;
+  }
+
+  Observer<KinTransaction> streamTransactions(KinAccountId kinAccountId) {
+
+
+    EventListener<TransactionResponse> listener = null;
+    
+    var subject = ValueSubject<KinTransaction>();
     listener = EventListener<TransactionResponse>() {
       onEvent(TransactionResponse data) {
         subject.onNext(data.asKinTransaction(environment.networkEnv));
@@ -40,5 +63,13 @@ class HorizonKinApi implements KinJsonApi {
     });
       
     return subject;
+  }
+
+  getAccount(GetAccountRequest request, GetAccountResponse onCompleted) {
+    KinAccount account = null;
+    var result = try {
+      server.accounts()
+        .account(request.accountId.toKeyPair());
+    }
   }
 }
