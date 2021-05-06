58bdf77daa5dca8d99ea648902e2159098ad63b0
gmpassos <gracilianomp@gmail.com>
2021-05-05 21:10:52 -0300

    Working implementation: - app_info_repository.dart - invoice_repository.dart

--- base/lib/base/repository/app_info_repository.dart
+++ base/lib/base/repository/app_info_repository.dart
@@ -1,1 +1,22 @@
-//Placeholder
+import 'package:kin_base/base/models/app_info.dart';
+import 'package:kin_base/base/models/appidx.dart';
+
+abstract class AppInfoRepository {
+  void addAppInfo(AppInfo appInfo);
+
+  Future<AppInfo> appInfoByAppIndex(AppIdx appIndex);
+}
+
+class InMemoryAppInfoRepositoryImpl implements AppInfoRepository {
+  final Map<AppIdx, AppInfo> _storage = <AppIdx, AppInfo>{};
+
+  @override
+  void addAppInfo(AppInfo appInfo) {
+    _storage[appInfo.appIndex] = appInfo;
+  }
+
+  @override
+  Future<AppInfo> appInfoByAppIndex(AppIdx appIndex) async {
+    return _storage[appIndex];
+  }
+}
