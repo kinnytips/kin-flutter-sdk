a11cfcbfca6ce617d8ab999b780d1666109e8ab7
gmpassos <gracilianomp@gmail.com>
2021-05-06 03:50:24 -0300

    kin_logger.dart: - KinLoggerImpl - KinLoggerFactoryImpl

--- base/lib/base/tools/kin_logger.dart
+++ base/lib/base/tools/kin_logger.dart
@@ -15,3 +15,50 @@ abstract class KinLoggerFactory {
 
   KinLogger getLogger(String name);
 }
+
+/// Basic logging implementation (prints to console).
+class KinLoggerImpl implements KinLogger {
+
+  final String name ;
+  @override
+  bool isLoggingEnabled;
+
+  KinLoggerImpl(this.name, [ this.isLoggingEnabled = false ]);
+
+  @override
+  void error(String msg, [Error throwable]) {
+    if (!isLoggingEnabled) return ;
+    print('[ERROR] $name> $msg');
+    print(error);
+  }
+
+  @override
+  void log(String msg) {
+    if (!isLoggingEnabled) return ;
+    print('[INFO] $name> $msg');
+  }
+
+  @override
+  void logByFunction(String Function() msg) {
+    log(msg());
+  }
+
+  @override
+  void warning(String msg) {
+    if (!isLoggingEnabled) return ;
+    print('[WARN] $name> $msg');
+  }
+
+}
+
+class KinLoggerFactoryImpl implements KinLoggerFactory {
+  @override
+  bool isLoggingEnabled;
+
+  KinLoggerFactoryImpl([ this.isLoggingEnabled = false ]);
+
+  @override
+  KinLogger getLogger(String name) {
+    return KinLoggerImpl(name, isLoggingEnabled);
+  }
+}
