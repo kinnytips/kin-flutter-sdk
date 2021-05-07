d505acf5ce54944ba0907b6bfadaa4e9c066c57f
Devayan Sarkar <devayansarkar@gmail.com>
2021-05-08 01:04:27 +0200

    adding skeleton kin_transaction for solana

--- base/lib/base/stellar/models/kin_transaction.dart
+++ base/lib/base/stellar/models/kin_transaction.dart
@@ -1,35 +1,73 @@
+import 'dart:typed_data';
 
 import 'package:kin_base/base/models/invoices.dart';
+import 'package:kin_base/base/models/kin_account.dart';
 import 'package:kin_base/base/models/kin_memo.dart';
 import 'package:kin_base/base/models/quark_amount.dart';
+import 'package:kin_base/base/models/solana/transaction.dart';
 import 'package:kin_base/base/models/transaction_hash.dart';
+import 'package:kin_base/base/stellar/models/network_environment.dart';
+import 'package:kin_base/base/stellar/models/record_type.dart';
 
 import 'kin_operation.dart';
 
-//TODO
-class RecordType {
-  int timeStamp;
+abstract class KinTransaction {
+  final Uint8List bytesValue;
+  final RecordType recordType;
+  final NetworkEnvironment networkEnvironment;
+  final QuarkAmount fee;
+  final KinMemo memo;
+  final TransactionHash transactionHash;
+  final List<KinOperationPayment> paymentOperations;
+  final InvoiceList invoiceList;
 
-  RecordType(this.timeStamp);
+  KinTransaction(
+      this.bytesValue,
+      this.recordType,
+      this.networkEnvironment,
+      this.fee,
+      this.memo,
+      this.transactionHash,
+      this.paymentOperations,
+      this.invoiceList);
 }
 
-class KinTransaction {
-  RecordType recordType;
-  QuarkAmount fee;
-  KinMemo memo;
-  TransactionHash transactionHash;
-  List<KinOperationPayment> paymentOperations ;
-  InvoiceList invoiceList;
+class SolanaKinTransaction extends KinTransaction {
+  final Uint8List _bytesValue;
+  final RecordType _recordType;
+  final NetworkEnvironment _networkEnvironment;
+  final QuarkAmount _fee;
+  final KinMemo _memo;
+  final TransactionHash _transactionHash;
+  final List<KinOperationPayment> _paymentOperations;
+  final InvoiceList _invoiceLis;
 
-  KinTransaction copyWithInvoiceList(InvoiceList invoiceList) {
-    //TODO
-  }
-}
+  SolanaKinTransaction(
+      this._bytesValue,
+      this._recordType,
+      this._networkEnvironment,
+      this._fee,
+      this._memo,
+      this._transactionHash,
+      this._paymentOperations,
+      this._invoiceLis)
+      : super(_bytesValue, _recordType, _networkEnvironment, _fee, _memo,
+            _transactionHash, _paymentOperations, _invoiceLis);
+  // TODO NOT able to use the Marshalling as present in the encoding.dart
+  // Kotlin files use the Companion Object to access the method directly
+  final _transaction = Transaction();
 
-class KinTransactionPagingToken {
+  get transactionHash =>
+      TransactionHash(_transaction.signatures.first.value.byteArray);
 
-}
+  get signingSource =>
+      KinAccountId(_transaction.message.accounts[0].asPublicKey().value);
 
-class SolanaKinTransaction {
+  get fee => QuarkAmount(0);
 
+  // TODO add Get Memo according to the Kotlin class
+  get memo => null;
+
+  // TODO add paymentOperations according to the Kotlin class
+  get paymentOperations => null;
 }
