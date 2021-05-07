2696cf6399ebc7e894b1ca0a6adcdfe746c77e78
Devayan Sarkar <devayansarkar@gmail.com>
2021-05-08 00:15:31 +0200

    result code for stellarfork

---
+++ base/lib/base/stellar/models/result_code.dart
@@ -0,0 +1,53 @@
+class ResultCode {
+  final int value;
+  ResultCode(this.value);
+}
+
+
+class Success extends ResultCode {
+  Success(): super(0);
+}
+
+class Failed extends ResultCode {
+  Failed(): super(-1);
+}
+
+class TooEarly extends ResultCode {
+  TooEarly(): super(-2);
+}
+
+class TooLate extends ResultCode {
+    TooLate(): super(-3);
+}
+
+class MissingOperation extends ResultCode {
+    MissingOperation(): super(-4);
+}
+
+class BadSequenceNumber extends ResultCode {
+    BadSequenceNumber(): super(-5);
+}
+
+class BadAuth extends ResultCode {
+    BadAuth(): super(-6);
+}
+
+class InsufficientBalance extends ResultCode {
+    InsufficientBalance(): super(-7);
+}
+
+class NoAccount extends ResultCode {
+    NoAccount(): super(-8);
+}
+
+class InsufficientFee extends ResultCode {
+    InsufficientFee(): super(-9);
+}
+
+class BadAuthExtra extends ResultCode {
+    BadAuthExtra(): super(-10);
+}
+
+class InternalError extends ResultCode {
+    InternalError(): super(-11);
+}
