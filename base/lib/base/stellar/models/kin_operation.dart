69919956d15d4df264ac499e98b4176f8613d496
gmpassos <gracilianomp@gmail.com>
2021-05-07 01:32:06 -0300

    basic of kin_transaction.dart

--- base/lib/base/stellar/models/kin_operation.dart
+++ base/lib/base/stellar/models/kin_operation.dart
@@ -1,1 +1,15 @@
-//Placeholder
+
+import 'package:kin_base/base/models/kin_account.dart';
+import 'package:kin_base/base/models/kin_amount.dart';
+
+class KinOperation {
+
+}
+
+class KinOperationPayment extends KinOperation {
+  final KinAmount amount;
+  final KinAccountId source;
+  final KinAccountId destination ;
+
+  KinOperationPayment(this.amount, this.source, this.destination);
+}
