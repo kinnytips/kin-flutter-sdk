417fbe9fdf46af81e8b52b6f8f0d9e9374fccd7c
gmpassos <gracilianomp@gmail.com>
2021-05-14 19:15:19 -0300

    - Moved `Transaction` operations to `TransactionExtension`, to keep original structure.   - fixed some operations.

--- base/lib/base/stellar/models/kin_transaction.dart
+++ base/lib/base/stellar/models/kin_transaction.dart
@@ -13,7 +13,53 @@ import 'package:kin_base/base/stellar/models/network_environment.dart';
 import 'package:kin_base/base/stellar/models/record_type.dart';
 import 'package:kin_base/base/tools/charsets.dart';
 
+import 'package:kin_base/base/models/stellar_base_type_conversions.dart';
+
 import 'kin_operation.dart';
+
+extension TransactionExtension on Transaction {
+  TransactionHash get transactionHash => TransactionHash(signatures.first.value.byteArray) ;
+
+  KinAccountId get signingSource => message.accounts[0].asKinAccountId() ;
+
+  QuarkAmount get fee => QuarkAmount(0);
+
+  KinMemo get memo {
+    var instruction = message.instructions.firstWhere((e) => message.accounts[e.programIndex] == MemoProgram.PROGRAM_KEY , orElse: () => null) ;
+    if (instruction == null) return KinMemo.none ;
+
+    var stringBase64 = utf8.decode(instruction.data);
+    var decodedBase64 = stringBase64.isNotEmpty ? base64.decode(stringBase64) : null ;
+    
+    if (decodedBase64 != null && decodedBase64.isNotEmpty) {
+      var memo = KinMemo( decodedBase64 , KinMemoTypeCharsetEncoded(Charset.utf8));
+
+      if (memo.getAgoraMemo() != null) {
+        return memo;
+      } else {
+        return KinMemo(instruction.data, KinMemoTypeCharsetEncoded(Charset.utf8));
+      }
+    }
+    else {
+      return KinMemo(instruction.data, KinMemoTypeCharsetEncoded(Charset.utf8));
+    }
+  }
+
+  List<KinOperationPayment> get paymentOperations {
+    return message.instructions.where((e) {
+      var programKey = message.accounts[e.programIndex] ;
+      return programKey != MemoProgram.PROGRAM_KEY
+          && programKey != SystemProgram.PROGRAM_KEY
+          && e.data.first.toInt() == TokenProgramCommandTransfer().value ;
+    }).map((e) {
+      var amountAsLong = e.data.buffer.asInt64List(1)[0] ;
+      var amount = QuarkAmount(amountAsLong).toKin() ;
+      var source = message.accounts[e.accounts[0]].asKinAccountId() ;
+      var destination = message.accounts[e.accounts[1]].asKinAccountId() ;
+      return KinOperationPayment(amount, source, destination);
+    }).toList();
+  }
+}
 
 abstract class KinTransaction {
   final Uint8List bytesValue;
@@ -29,110 +75,40 @@ abstract class KinTransaction {
       this.bytesValue,
       this.recordType,
       this.networkEnvironment,
+      [this.invoiceList,
       this.fee,
       this.memo,
       this.transactionHash,
-      this.paymentOperations,
-      this.invoiceList);
+      this.paymentOperations,]
+      );
 
   KinTransaction copyWithInvoiceList(InvoiceList invoiceList) ;
 }
 
 class SolanaKinTransaction extends KinTransaction {
-  final Uint8List _bytesValue;
-  final RecordType _recordType;
-  final NetworkEnvironment _networkEnvironment;
-  final QuarkAmount _fee;
-  final KinMemo _memo;
-  final TransactionHash _transactionHash;
-  final List<KinOperationPayment> _paymentOperations;
-  final InvoiceList _invoiceLis;
+
   Transaction _transaction;
 
   SolanaKinTransaction(
-      this._bytesValue,
-      this._recordType,
-      this._networkEnvironment,
-      this._fee,
-      this._memo,
-      this._transactionHash,
-      this._paymentOperations,
-      this._invoiceLis)
-      : super(_bytesValue, _recordType, _networkEnvironment, _fee, _memo,
-            _transactionHash, _paymentOperations, _invoiceLis) {
-    this._transaction = Transaction.unmarshal(_bytesValue);
+      Uint8List bytesValue,
+      RecordType recordType,
+      NetworkEnvironment networkEnvironment,
+      InvoiceList invoiceLis)
+      : super(bytesValue, recordType, networkEnvironment, invoiceLis) {
+    this._transaction = Transaction.unmarshal(bytesValue);
   }
+
+  TransactionHash get transactionHash => _transaction.transactionHash ;
+  KinAccountId get signingSource => _transaction.signingSource ;
+
+  QuarkAmount get fee => _transaction.fee ;
+
+  KinMemo get memo => _transaction.memo ;
+
+  List<KinOperationPayment> get paymentOperations => _transaction.paymentOperations ;
 
   SolanaKinTransaction copyWithInvoiceList(InvoiceList invoiceList) {
-    return SolanaKinTransaction(_bytesValue, _recordType, _networkEnvironment,
-        _fee, _memo, _transactionHash, _paymentOperations, invoiceList);
+    return SolanaKinTransaction(bytesValue, recordType, networkEnvironment, invoiceList);
   }
 
-  get transactionHash =>
-      TransactionHash(_transaction.signatures.first.value.byteArray);
-
-  get signingSource =>
-      KinAccountId(_transaction.message.accounts[0].asPublicKey().value);
-
-  get fee => QuarkAmount(0);
-
-  KinMemo get memo {
-    CompiledInstruction compiledInstruction;
-
-    for (CompiledInstruction instruction in _transaction.message.instructions) {
-      if (_transaction.message.accounts[instruction.programIndex] ==
-          MemoProgram.PROGRAM_KEY) {
-        compiledInstruction = instruction;
-        break;
-      }
-    }
-
-    if (compiledInstruction == null) {
-      return KinMemo.none;
-    }
-    var base64Decoded = base64.decode(base64.encode(compiledInstruction.data));
-
-    if (base64Decoded == null) {
-      return KinMemo.fromText(
-          String.fromCharCodes(compiledInstruction.data), Charset.utf8);
-    }
-
-    var memo = KinMemo(base64Decoded);
-
-    if (memo.getAgoraMemo == null) {
-      return KinMemo(
-          compiledInstruction.data, KinMemoTypeCharsetEncoded(Charset.utf8));
-    }
-
-    return memo;
-  }
-
-  List<KinOperationPayment> get paymentOperations {
-    List<CompiledInstruction> instructions = [];
-
-    _transaction.message.instructions.forEach((instruction) {
-      var programKey = _transaction.message.accounts[instruction.programIndex];
-
-      if (programKey != MemoProgram.PROGRAM_KEY &&
-          programKey != SystemProgram.PROGRAM_KEY &&
-          instruction.data.first == TokenProgramCommandTransfer().value) {
-        instructions.add(instruction);
-      }
-    });
-
-    List<KinOperationPayment> payments = [];
-    for (CompiledInstruction instruction in instructions) {
-      var amount = QuarkAmount(
-              instruction.data.sublist(1, instruction.data.length - 1).hashCode)
-          .toKin();
-      var source =
-          _transaction.message.accounts[instruction.accounts[0]].asPublicKey();
-      var destination =
-          _transaction.message.accounts[instruction.accounts[1]].asPublicKey();
-      payments.add(KinOperationPayment(
-          amount, KinAccountId(source.value), KinAccountId(destination.value)));
-    }
-
-    return payments;
-  }
 }
