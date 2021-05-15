d8334373292fdbadf1085337d3642351db585c09
gmpassos <gracilianomp@gmail.com>
2021-05-14 22:42:24 -0300

    added `kin_account_api_v4.dart`

--- base/lib/base/network/api/kin_account_api_v4.dart
+++ base/lib/base/network/api/kin_account_api_v4.dart
@@ -1,1 +1,12 @@
-//Placeholder
+
+import 'package:kin_base/base/models/key.dart';
+import 'package:kin_base/base/models/kin_account.dart';
+import 'package:kin_base/base/network/services/kin_service.dart';
+
+import 'kin_account_api.dart';
+
+abstract class KinAccountApiV4 extends KinAccountApi {
+
+  Future<KinServiceResponse<List<PublicKey>>> resolveTokenAccounts(KinAccountId accountId) ;
+
+}
