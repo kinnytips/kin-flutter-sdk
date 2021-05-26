2867f26ddeb4bf6e67438d89e9e349aec37ba4a3
gmpassos <gracilianomp@gmail.com>
2021-05-26 02:35:40 -0300

    - Implement `KinServiceImplV4.createAccount`.

--- base/lib/base/network/services/kin_service_impl_v4.dart
+++ base/lib/base/network/services/kin_service_impl_v4.dart
@@ -1,24 +1,28 @@
 
+import 'dart:async';
 
 import 'package:kin_base/base/models/key.dart';
 import 'package:kin_base/base/models/kin_account.dart';
 import 'package:kin_base/base/models/kin_memo.dart';
 import 'package:kin_base/base/models/kin_payment_item.dart';
 import 'package:kin_base/base/models/quark_amount.dart';
+import 'package:kin_base/base/models/solana/programs.dart';
+import 'package:kin_base/base/models/solana/transaction.dart';
+import 'package:kin_base/base/models/stellar_base_type_conversions.dart';
 import 'package:kin_base/base/models/transaction_hash.dart';
-import 'package:kin_base/base/network/api/kin_account_api.dart';
 import 'package:kin_base/base/network/api/kin_account_api_v4.dart';
 import 'package:kin_base/base/network/api/kin_account_creation_api_v4.dart';
 import 'package:kin_base/base/network/api/kin_streaming_api_v4.dart';
-import 'package:kin_base/base/network/api/kin_transaction_api.dart';
 import 'package:kin_base/base/network/api/kin_transaction_api_v4.dart';
 import 'package:kin_base/base/stellar/models/kin_transaction.dart';
 import 'package:kin_base/base/stellar/models/network_environment.dart';
 import 'package:kin_base/base/stellar/models/paging_token.dart';
+import 'package:kin_base/base/tools/byte_utils.dart';
 import 'package:kin_base/base/tools/cache.dart';
 import 'package:kin_base/base/tools/kin_logger.dart';
 import 'package:kin_base/base/tools/network_operations_handler.dart';
 import 'package:kin_base/base/tools/observers.dart';
+import 'package:kin_base/stellarfork/key_pair.dart';
 
 import 'kin_service.dart';
 
@@ -43,9 +47,34 @@ class KinServiceImplV4 extends KinService {
 
   final Cache<String> _cache = Cache<String>();
 
+  Future<KinServiceResponse<ServiceConfig>> _cachedServiceConfig() async {
+    return await _cache.resolve("serviceConfig", timeoutOverride: Duration(minutes: 30), fault: (key) {
+      return networkOperationsHandler.queueWork('_cachedServiceConfig', () async {
+        return await transactionApi.getServiceConfig();
+      });
+    });
+  }
+
+  Future<KinServiceResponse<Hash>> _cachedRecentBlockHash() async {
+    return await _cache.resolve("recentBlockHash", timeoutOverride: Duration(minutes: 2), fault: (key) {
+      return networkOperationsHandler.queueWork('_cachedServiceConfig', () async {
+        return await transactionApi.getRecentBlockHash();
+      });
+    });
+  }
+
+
+  Future<KinServiceResponse<int>> _cachedMinRentExemption() async {
+    return await _cache.resolve("minRentExemption", timeoutOverride: Duration(minutes: 30), fault: (key) {
+      return networkOperationsHandler.queueWork('_cachedServiceConfig', () async {
+        return await transactionApi.getMinimumBalanceForRentExemption( TokenProgram().accountSize );
+      });
+    });
+  }
+
   @override
   Future<KinAccount> getAccount(KinAccountId accountId) {
-
+    throw UnimplementedError('!!!getAccount');
   }
 
   @override
@@ -93,8 +122,81 @@ class KinServiceImplV4 extends KinService {
 
   @override
   Future<KinAccount> createAccount(KinAccountId accountId, PrivateKey signer) {
-    // TODO: implement createAccount
-    throw UnimplementedError();
+    return networkOperationsHandler.queueWork('accountApi.resolveTokenAccounts', () async {
+      var ret = await Future.wait( [ _cachedServiceConfig() , _cachedRecentBlockHash(), _cachedMinRentExemption() ] );
+      var serviceConfig = ret[0].payload as ServiceConfig ;
+      var recentBlockHash = ret[1].payload as Hash ;
+      var minRentExemption = ret[2].payload as int ;
+
+      var tokenAccountSeed = signer.toSigningKeyPair().rawSecretSeed.toSha256() ;
+      var tokenAccount = KeyPair.fromSecretSeedBytes(tokenAccountSeed).asPrivateKey();
+      var tokenAccountPub = tokenAccount.asPublicKey();
+
+      var subsidizer = serviceConfig.subsidizerAccount.toKeyPair().asPublicKey();
+      var owner = signer.asPublicKey();
+      var programKey = serviceConfig.tokenProgram.toKeyPair().asPublicKey();
+      var mint = serviceConfig.token.toKeyPair().asPublicKey();
+
+      var transaction = Transaction.newTransaction(subsidizer, [
+        CreateAccount(
+          subsidizer,
+          tokenAccountPub,
+          programKey,
+          minRentExemption,
+          TokenProgram().accountSize,
+        ).instruction,
+        TokenProgramInitializeAccount(
+          tokenAccountPub,
+          mint,
+          owner,
+          programKey,
+        ).instruction,
+        SetAuthority(
+          tokenAccountPub,
+          owner,
+          subsidizer,
+          TokenProgramAuthorityTypeCloseAccount(),
+          programKey,
+        ).instruction
+      ]).copyAndSetRecentBlockhash(recentBlockHash).copyAndSign([tokenAccount, signer]);
+
+      var response = await accountCreationApi.createAccountV4(transaction) ;
+
+      if (response.type == KinServiceResponseType.ok) {
+        var account = response.payload;
+        if (account != null) {
+          return account ;
+        } else {
+          throw IllegalResponseError();
+        }
+      }
+      else if (response.type == KinServiceResponseType.exists) {
+        var account = response.payload;
+        if (account != null) {
+          return account ;
+        } else {
+          return getAccount(accountId);
+        }
+      }
+      else if (response.type == KinServiceResponseType.badNonce) {
+        throw TransientFailure(StateError('Bad Nonce: RecentBlockhash invalid')) ;
+      }
+      else if (response.type == KinServiceResponseType.transientFailure) {
+        throw TransientFailure(response.error) ;
+      }
+      else if (response.type == KinServiceResponseType.undefinedError) {
+        throw UnexpectedServiceError(response.error) ;
+      }
+      else if (response.type == KinServiceResponseType.upgradeRequiredError) {
+        throw SDKUpgradeRequired();
+      }
+      else if (response.type == KinServiceResponseType.payerRequired) {
+        throw UnexpectedServiceError(StateError('PayerRequired: no subsidizer set')) ;
+      }
+      else {
+        throw StateError("Can't handle response type: ${response.type}");
+      }
+    });
   }
 
   @override
@@ -116,9 +218,8 @@ class KinServiceImplV4 extends KinService {
   }
 
   @override
-  invalidateBlockhashCache() {
-    // TODO: implement invalidateBlockhashCache
-    throw UnimplementedError();
+  void invalidateBlockhashCache() {
+      _cache.invalidate("recentBlockHash");
   }
 
   @override
