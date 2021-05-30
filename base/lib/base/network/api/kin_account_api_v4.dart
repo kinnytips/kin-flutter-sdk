ae1f5a9b06f4d355f7caa2d727b85ca00800422f
gmpassos <gracilianomp@gmail.com>
2021-05-30 04:14:23 -0300

    - `resolveTokenAccounts`: fix return to `List<PublicKey>`. Can't expose `AccountInfo`, a protobuf implementation class.

--- base/lib/base/network/api/kin_account_api_v4.dart
+++ base/lib/base/network/api/kin_account_api_v4.dart
@@ -2,12 +2,11 @@
 import 'package:kin_base/base/models/key.dart';
 import 'package:kin_base/base/models/kin_account.dart';
 import 'package:kin_base/base/network/services/kin_service.dart';
-import 'package:kin_base/models/agora/protobuf/account/v4/account_service.pb.dart';
 
 import 'kin_account_api.dart';
 
 abstract class KinAccountApiV4 extends KinAccountApi {
 
-  Future<KinServiceResponse<List<AccountInfo>>> resolveTokenAccounts(KinAccountId accountId) ;
+  Future<KinServiceResponse<List<PublicKey>>> resolveTokenAccounts(KinAccountId accountId) ;
 
 }
