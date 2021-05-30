14013e7d00505b5d61c464234a72251ff56a0dca
gmpassos <gracilianomp@gmail.com>
2021-05-30 04:15:39 -0300

    - `getAccount`: implement error handling. - `resolveTokenAccounts`: implementation should return `List<PublicKey>`.

--- base/lib/base/network/api/agora/agora_v4_apis.dart
+++ base/lib/base/network/api/agora/agora_v4_apis.dart
@@ -79,35 +79,48 @@ class AgoraKinAccountApiV4 extends GrpcApi implements KinAccountApiV4, KinStream
 
   @override
   Future<KinServiceResponse<KinAccount>> getAccount(KinAccountId accountId) async {
-    model_v4.Commitment commitment = model_v4.Commitment.SINGLE ;
+    try {
+      model_v4.Commitment commitment = model_v4.Commitment.SINGLE;
 
-    var request = new GetAccountInfoRequest(accountId: accountId.toProtoSolanaAccountId(), commitment: commitment);
-    var account = await _accountClient.getAccountInfo(request);
+      var request = new GetAccountInfoRequest(
+          accountId: accountId.toProtoSolanaAccountId(),
+          commitment: commitment);
 
-    if(account.result == GetAccountInfoResponse_Result.OK)
-    {
-      var accountInfo = account.accountInfo.toKinAccount();
-      return KinServiceResponse(KinServiceResponseType.ok, accountInfo);
+      var account = await _accountClient.getAccountInfo(request);
+
+      if (account.result == GetAccountInfoResponse_Result.OK) {
+        var accountInfo = account.accountInfo.toKinAccount();
+        return KinServiceResponse(KinServiceResponseType.ok, accountInfo);
+      }
+      else if (account.result == GetAccountInfoResponse_Result.NOT_FOUND) {
+        return KinServiceResponse(KinServiceResponseType.notFound);
+      }
+      else {
+        return KinServiceResponse(KinServiceResponseType.undefinedError, null,
+            UnrecognizedResultException(UnrecognizedProtoResponse()));
+      }
     }
-    else if (account.result == GetAccountInfoResponse_Result.NOT_FOUND)
-    {
-      return KinServiceResponse(KinServiceResponseType.notFound);
-    }
-    else
-    {
-      return KinServiceResponse(KinServiceResponseType.undefinedError, null, UnrecognizedResultException(UnrecognizedProtoResponse()) );
+    catch (e) {
+      if ( GrpcApi.canRetry(e) ) {
+        return KinServiceResponse(KinServiceResponseType.transientFailure, null, TransientFailure(e));
+      }
+      else if ( GrpcApi.isForcedUpgrade(e) ) {
+        return KinServiceResponse(KinServiceResponseType.upgradeRequiredError, null);
+      }
+      else {
+        return KinServiceResponse(KinServiceResponseType.undefinedError, null, UnrecognizedResultException(e));
+      }
     }
   }
 
   @override
-  Future<KinServiceResponse<List<AccountInfo>>> resolveTokenAccounts(KinAccountId accountId) async {
-    
-  model_v4.Commitment commitment = model_v4.Commitment.SINGLE ;
-
-    var request = new ResolveTokenAccountsRequest(accountId: accountId.toProtoSolanaAccountId(), includeAccountInfo: true)
+  Future<KinServiceResponse<List<PublicKey>>> resolveTokenAccounts(KinAccountId accountId) async {
+    var request = new ResolveTokenAccountsRequest(accountId: accountId.toProtoSolanaAccountId(), includeAccountInfo: true);
     var accounts = await _accountClient.resolveTokenAccounts(request);
 
-    return new KinServiceResponse(KinServiceResponseType.ok, accounts.tokenAccountInfos);
+    var publicKeys = accounts.tokenAccountInfos.map((e) => e.accountId.toPublicKey()).toList();
+
+    return new KinServiceResponse(KinServiceResponseType.ok, publicKeys);
   }
 
   @override
