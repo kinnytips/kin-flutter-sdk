76c3bc112dbdf3c5049a26789112424f87d7ecb2
gmpassos <gracilianomp@gmail.com>
2021-06-22 19:14:44 -0300

    fix `if (dart.library.html) "file_storage_web.dart"`

--- base/lib/base/storage/file_storage.dart
+++ base/lib/base/storage/file_storage.dart
@@ -2,7 +2,7 @@ import 'dart:typed_data';
 
 import 'file_storage_generic.dart'
     if (dart.library.io) "file_storage_io.dart"
-    if (dart.library.html) "file_storage_html.dart";
+    if (dart.library.html) "file_storage_web.dart";
 
 abstract class FileStorage {
   static FileStorage create() => createFileStorage();
